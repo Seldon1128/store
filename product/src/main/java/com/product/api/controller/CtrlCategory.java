26a5bb650193c282d32a5b982496d8d73fdb71b2
seldon1128 <seldon1128@gmail.com>
2025-09-03 19:32:00 -0600

    actualizando controlador

--- product/src/main/java/com/product/api/controller/CtrlCategory.java
+++ product/src/main/java/com/product/api/controller/CtrlCategory.java
@@ -7,23 +7,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.product.api.entity.Category;
+import com.product.api.service.SvcCategory; 
 
 @RestController
 @RequestMapping("/category")
 public class CtrlCategory {
 	@Autowired
+	private final SvcCategory svc;
+	
+	public CtrlCategory(SvcCategory svc) {
+        this.svc = svc;
+    }
+	
 	
 	@GetMapping
 	public List<Category> getCategories(){
 		//Lista de categorias
-		List<Category> categories = new ArrayList<Category>();
+		//List<Category> categories = new ArrayList<Category>();
 		//categories.add(new Category(1,"Lentes","Lts",1));
 		//categories.add(new Category(2,"Relojes","Rljs",1));
-		//categories.add(new Category(3,"Gorras","Grs",1));
-		//categories.add(new Category(4,"Mochilas","Mch",1));
-		//categories.add(new Category(5,"Pulseras","Pls",1));
-		//categories.add(new Category(6,"Carteras","Ctrs",0));
 		
-		return categories;
+		//return categories;
+		return svc.getCategories();
 	}
 }
