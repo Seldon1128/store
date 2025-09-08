f4a90465324a60cb44b34159a3a11854d8da5939
seldon1128 <seldon1128@gmail.com>
2025-09-08 17:29:51 -0600

    Practica completa

--- product/src/main/java/com/product/api/controller/CtrlCategory.java
+++ product/src/main/java/com/product/api/controller/CtrlCategory.java
@@ -3,6 +3,7 @@ package com.product.api.controller;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,15 +14,11 @@ import com.product.api.service.SvcCategory;
 @RequestMapping("/category")
 public class CtrlCategory {
 	@Autowired
-	private final SvcCategory svc;
-	
-	public CtrlCategory(SvcCategory svc) {
-        this.svc = svc;
-    }
+	SvcCategory svc;
 	
 	
 	@GetMapping
-	public List<Category> getCategories(){
+	public ResponseEntity<List<Category>> getCategories(){
 		//Lista de categorias
 		//List<Category> categories = new ArrayList<Category>();
 		//categories.add(new Category(1,"Lentes","Lts",1));
